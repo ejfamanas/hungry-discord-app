cc9cc29ecfb4ebe68e9664bd73b097095818433f
Eric Famanas <efamanas@crfs.com>
2021-02-13 15:40:17 +0000

    adds responsiveness to bot

--- src/bot.ts
+++ src/bot.ts
@@ -15,9 +15,27 @@ export class Bot {
         this.token = token;
     }
 
-    public listen(): Promise < string > {
-        this.client.on('message', (message: Message): void => {
+    public async listen(): Promise<string> {
+        await this.client.on('message', (message: Message): void => {
             console.log("Message received! Contents: ", message.content);
+            const prefix = "!";
+
+            if (message.author.bot) {
+                return;
+            }
+            if (!message.content.startsWith(prefix)) {
+                return;
+            }
+
+            const commandBody = message.content.slice(prefix.length);
+            const args = commandBody.split(' ');
+            const command = args.shift()!.toLowerCase();
+            if (command === "ping") {
+                const timeTaken = Date.now() - message.createdTimestamp;
+                message.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
+            } else {
+                message.reply(`Command: ${command} is not a known command. Please try again`);
+            }
         });
 
         return this.client.login(this.token);
