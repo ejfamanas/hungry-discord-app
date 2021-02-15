e347f4e758032a35dbae97cecdec3bcd1d27d36e
Eric Famanas <efamanas@crfs.com>
2021-02-15 10:18:08 +0000

    updates, fixes bugs

--- src/bot.ts
+++ src/bot.ts
@@ -1,41 +1,37 @@
 import {Client, Message} from "discord.js";
 import {inject, injectable} from "inversify";
 import {TYPES} from "./types";
+import {MessageResponder} from "./services/message-responder";
 
 @injectable()
 export class Bot {
     private client: Client;
     private readonly token: string;
+    private messageResponder: MessageResponder;
 
     constructor(
         @inject(TYPES.Client) client: Client,
-        @inject(TYPES.Token) token: string
-    ) {
+        @inject(TYPES.Token) token: string,
+        @inject(TYPES.MessageResponder) messageResponder: MessageResponder) {
         this.client = client;
         this.token = token;
+        this.messageResponder = messageResponder;
     }
 
-    public async listen(): Promise<string> {
-        await this.client.on('message', (message: Message): void => {
-            console.log("Message received! Contents: ", message.content);
-            const prefix = "!";
-
+    public listen(): Promise<string> {
+        this.client.on('message', (message: Message) => {
             if (message.author.bot) {
-                return;
-            }
-            if (!message.content.startsWith(prefix)) {
+                console.log('Ignoring bot message!')
                 return;
             }
 
-            const commandBody = message.content.slice(prefix.length);
-            const args = commandBody.split(' ');
-            const command = args.shift()!.toLowerCase();
-            if (command === "ping") {
-                const timeTaken = Date.now() - message.createdTimestamp;
-                message.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
-            } else {
-                message.reply(`Command: ${command} is not a known command. Please try again`);
-            }
+            console.log("Message received! Contents: ", message.content);
+
+            this.messageResponder.handle(message).then(() => {
+                console.log("Response sent!");
+            }).catch(() => {
+                console.log("Response not sent.")
+            })
         });
 
         return this.client.login(this.token);
