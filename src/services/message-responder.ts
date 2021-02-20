b97602dc1b2acbcd8985fdffbdbd41ca82196b87
Eric Famanas <efamanas@crfs.com>
2021-02-20 19:58:02 +0000

    adds dockerfile, basic unit tests example

--- src/services/message-responder.ts
+++ src/services/message-responder.ts
@@ -17,11 +17,21 @@ export class MessageResponder {
         if (this.pingFinder.isPing(message.content)) {
             return message.reply('pong!');
         }
-        if (message.content.includes("$whoami")) {
+        if (message.content.includes("whoami")) {
             const {username} = message.author;
             return message.reply(username === "fanman"
-                ? `${username} is the greatest`
-                : `${username} is also the greatest`);
+                ? `${username} is my name`
+                : username === "Always Hungry" || "Si2S0lo"
+                    ? `${username} is also my name`
+                : `${username} change your name`);
+        }
+        if (message.content.includes("give me a square")) {
+            const {username} = message.author;
+            return message.reply(username === "fanman"
+                ? `gives ${username} a square`
+                : username === "Always Hungry" || "Si2S0lo" || "Tony V"
+                    ? `bums ${username} a square`
+                    : `${username} change your name`);
         }
         return Promise.reject();
     }
