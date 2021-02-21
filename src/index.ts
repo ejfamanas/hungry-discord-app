40c87016ca58b390535caca22c937f0a2c60c71e
Eric Famanas <efamanas@crfs.com>
2021-02-21 22:12:57 +0000

    removes test

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,10 @@
 import container from "./inversify.config";
 import {TYPES} from "./types";
 import {Bot} from "./bot";
-import HTML = Mocha.reporters.HTML;
+
 let bot = container.get<Bot>(TYPES.Bot);
 bot.listen().then(() => {
     console.log('Logged in!')
 }).catch((error) => {
     console.log('Oh no! ', error)
 });
-
-// @ts-ignore
-document.append((<div>Hello world</div>) as HTMLElement);
