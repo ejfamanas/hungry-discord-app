44c6e887689f21a0633c63b9060af64f675459fc
Eric Famanas <efamanas@crfs.com>
2021-02-21 22:08:51 +0000

    experiment with  html

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import container from "./inversify.config";
 import {TYPES} from "./types";
 import {Bot} from "./bot";
+import HTML = Mocha.reporters.HTML;
 let bot = container.get<Bot>(TYPES.Bot);
 bot.listen().then(() => {
     console.log('Logged in!')
@@ -8,8 +9,5 @@ bot.listen().then(() => {
     console.log('Oh no! ', error)
 });
 
-function render(): string {
-    return "Hello world";
-}
-
-render();
+// @ts-ignore
+document.append((<div>Hello world</div>) as HTMLElement);
